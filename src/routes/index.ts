d76c32160c546c1ab0fa1e99c787636ed5d0ccb3
Benjamin VIELLARD <bicou@bicou.com>
2023-02-06 21:05:20 +0100

    chore: try nitropack

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import { createYoga } from 'graphql-yoga';
+import { schema } from "../schema";
+
+// Create a Yoga instance with a GraphQL schema.
+const yoga = createYoga({
+    schema,
+    graphqlEndpoint: '/'
+});
+
+export default defineEventHandler((event) => yoga.handle(event.node.req, event.node.res))
