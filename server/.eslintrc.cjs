85f2367da69b1046e749e87fa07083bfc797d6f3
Benjamin VIELLARD <bicou@bicou.com>
2023-06-16 16:37:29 +0200

    chore: eslint simple config name

--- server/.eslintrc.cjs
+++ server/.eslintrc.cjs
@@ -3,7 +3,7 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   root: true,
   extends: [
-    "eslint-config-bicou",
+    "bicou",
   ],
   parserOptions: {
     tsconfigRootDir: __dirname,
